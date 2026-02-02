fad60c4040d98945ffec653ca488fe45c4a3ca97
haku7a <v.a.neyaskin@gmail.com>
2026-02-03 02:21:56 +0300

    chore: setup logging configuration

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+)
+
+
 def main():
     print("Hello, World!")
 
